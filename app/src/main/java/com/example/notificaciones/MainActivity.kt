d2452984ee1c2d1f7d96569846afa7a2860b2965
JesusLopez28 <jesus.lopez280402@gmail.com>
2024-09-04 08:32:03 -0600

    Initial commit

---
+++ app/src/main/java/com/example/notificaciones/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.notificaciones
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM", "Fetching FCM registration token failed", task.exception)
+                return@addOnCompleteListener
+            }
+
+            // Obtener el token
+            val token = task.result
+            Log.d("FCM", "Token: $token")
+            // Aquí puedes enviar el token a tu servidor si es necesario
+        }
+
+    }
+}
